7be765b846de35307fb5df6fbd0d2b0c52b4c812
Happy-Grace <ichigozirim@gmail.com>
2025-08-24 03:33:19 +0000

    Week 4Challenge and Assignment Done!

---
+++ week4/week4_challenge.sql
@@ -0,0 +1,14 @@
+-- 1. Write an SQL query to show the total payment amount for each payment date 
+-- from payments table. (use sampleDB shared during week 2 class)🏫 💥
+
+-- Display the payment date and the total amount paid on that date.
+-- Sort the results by payment date in descending order.
+-- Show only the top 5 latest payment dates.
+
+USE salesdb;
+
+SELECT paymentDate, SUM(amount) AS totalAmount
+FROM payments
+GROUP BY paymentDate
+ORDER BY paymentDate DESC
+LIMIT 5;
